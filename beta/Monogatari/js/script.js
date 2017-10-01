8ac3b7c898005dc2fcdefd19f09fddc7539ecb3d
Cg Ting <cgtbulatao@gmail.com>
2017-10-01 16:30:19 +0800

    Changed #2 question in pronouns

--- beta/Monogatari/js/script.js
+++ beta/Monogatari/js/script.js
@@ -254,21 +254,21 @@ var script = {
 	"QuestionPronounsTwo":[
 		"Now for question #2",
 		"change QuestionPronounsThree",
-		{"Choice":
-			{
-				"Dialog": "sachi What is the pronoun in the sentence: On saturday, I will meet Joe at three o'clock.",
+		{
+			"Choice":{
+				"Dialog": "sachi What is the pronoun in the sentence: Jesse is a fan of soccer. He even went to a game once.",
 				"Correct":{
-					"Text":"I",
-					"Do":"jump Correct"
+					"Text": "He",
+					"Do": "jump Correct"
 				},
 				"Incorrect":{
-					"Text":"Joe",
-					"Do":"jump Incorrect"
+					"Text": "We",
+					"Do": "jump Incorrect"
 				}
 			}
 		}
-		
-	],
+	]
+	,
 	"QuestionPronounsThree":[
 		"Now for question #3",
 		"change QuestionPronounsFour",
